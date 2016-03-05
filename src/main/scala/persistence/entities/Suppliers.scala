4b68612097b38db5b5be0d3aefcc7cf4b52127ef
Cláudio Diniz <claudio.diniz@enear.co>
2016-03-05 11:51:36 +0000

    indentation fix

--- src/main/scala/persistence/entities/Suppliers.scala
+++ src/main/scala/persistence/entities/Suppliers.scala
@@ -2,9 +2,9 @@ package persistence.entities
 
 import slick.driver.H2Driver.api._
 
-case class Supplier(id: Long,name: String,desc: String) extends BaseEntity
+case class Supplier(id: Long, name: String, desc: String) extends BaseEntity
 
-case class SimpleSupplier(name: String,desc: String)
+case class SimpleSupplier(name: String, desc: String)
 
 class SuppliersTable(tag: Tag) extends BaseTable[Supplier](tag, "SUPPLIERS") {
   def name = column[String]("userID")
